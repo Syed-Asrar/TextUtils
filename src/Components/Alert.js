7c9d53313e2a976744b96dc725b83a4aed4ab020
Syed Asrar Zahoor <81914950+syedasrar00@users.noreply.github.com>
2024-02-13 16:23:02 +0530

    Update Alert.js

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alert(props) {
   return (
-    props.alert && <div className={`position-absolute alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    props.alert && <div className={`position-fixed alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{props.alert.type}</strong>. {props.alert.message}
     </div>
   )
